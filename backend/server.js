6ff1f5d97dc717fcb409794d3c01edfb9e603d53
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-27 15:17:39 +0100

    Add prot

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
